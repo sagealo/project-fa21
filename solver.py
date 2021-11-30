567915a0aed58064ebe72909aae3c9bd4373960d
Sage <sagepierone24@gmail.com>
2021-11-29 21:25:41 -0800

    Added a short preface to solver.py

--- solver.py
+++ solver.py
@@ -1,6 +1,24 @@
 from parse import read_input_file, write_output_file
 import os
 
+
+# Each task(igloo) has:
+#     - task_id (int): task id of the Task 
+#         !-> self.get_task_id()
+#     - deadline (int): deadline of the Task 
+#         !-> self.get_deadline()
+#     - duration (int): duration of the Task 
+#         !-> self.get_duration()
+#     - perfect_benefit (float): the benefit recieved from completing the Task anytime before (or on) the deadline 
+#         !-> self.get_max_benefit()
+# A task's profits decay exponentially according to the function:
+#     - p_i * (e ** (-0.0170 * s_i)), 
+#         where p_i is the perfect_benefit and s_i is the number of minutes late the task was completed
+#     !-> self.get_late_benefit(minutes_late)
+
+# Goal: We wish to maximize the total profit/benefit! 
+
+
 def solve(tasks):
     """
     Args:
