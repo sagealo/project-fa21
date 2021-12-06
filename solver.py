3ad34bd53bcbed2c39fe08b932dd9660021a911d
Abel Feleke <afcal@berkeley.edu>
2021-12-05 16:15:11 -0800

    heapify again

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@ from math import e
 import os
 import Task
 import random
+import heapq as heap
 
 
 # Each task(igloo) has:
@@ -56,6 +57,16 @@ def solve(tasks):
         tasks.remove(best)
 
     return result
+
+
+def heapify_tasks(tasks):
+    """Returns list of tuples (start_time, task) sorted by latest possible start time"""
+    start_times = []
+    for task in tasks:
+        start_time = max(0, task.get_deadline() - task.get_duration)
+        pair = (start_time, task)
+        heap.heappush(start_times, pair)
+    return start_times
 
 def output_profit(tasks):
     profit = 0
